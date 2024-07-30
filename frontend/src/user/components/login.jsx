f492cd48c82bc4b1328d2f5a076c45656d011894
Arbaz <arbaz582021@gmail.com>
2024-07-30 02:19:58 -0400

    add

--- frontend/src/user/components/login.jsx
+++ frontend/src/user/components/login.jsx
@@ -34,7 +34,7 @@ export default function SignIn() {
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'blue' }}>
+          <Avatar sx={{ m: 1, bgcolor: '#2563eb' }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
