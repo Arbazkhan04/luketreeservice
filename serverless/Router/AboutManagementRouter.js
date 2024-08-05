5c507ee1b2f681b467f2e1c98c97e3e7e7bbcd9b
Arbaz <arbaz582021@gmail.com>
2024-08-05 02:41:02 -0400

    add

--- serverless/Router/AboutManagementRouter.js
+++ serverless/Router/AboutManagementRouter.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const { getAboutData, createAboutData , updateAboutData} = require('../controller/AboutManagementController');
+const {protect} = require('../Middleware/authMiddelware')
 
 router.get('/getAboutData', getAboutData);
-router.post('/createAboutData', createAboutData);
-router.put('/updateAboutData/:aboutId',updateAboutData);
+router.post('/createAboutData',protect, createAboutData);
+router.put('/updateAboutData/:aboutId',protect,updateAboutData);
+
 
 module.exports = router;
