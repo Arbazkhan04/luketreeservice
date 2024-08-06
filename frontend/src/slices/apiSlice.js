5a62ee35feb12467285fce04be49df88d8603270
Arbaz <arbaz582021@gmail.com>
2024-08-06 02:12:49 -0400

    add

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://cffp5kcmld.execute-api.us-east-1.amazonaws.com', credentials: 'include', });
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://cffp5kcmld.execute-api.us-east-1.amazonaws.com' });
 
 export const apiSlice = createApi({
   baseQuery,
