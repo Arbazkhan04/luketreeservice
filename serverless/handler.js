2cf1ab09c76d55cd13b120a410100353a4570813
Arbaz <arbaz582021@gmail.com>
2024-08-06 01:25:45 -0400

    add

--- serverless/handler.js
+++ serverless/handler.js
@@ -2,12 +2,26 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const cookieParser = require("cookie-parser");
-
+const cors = require('cors');
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+const corsOptions = {
+  origin: 'http://localhost:3000', // or specify allowed origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Set-Cookie',
+  credentials: true,
+
+}
+app.use(cors(corsOptions));
 app.use(express.json());
+
+
+// Start Block Setting th Headers for your Application
+
+//End Block Setting the Header for your Application
 
 // Import the router
 const AboutManagementRouter = require('./Router/AboutManagementRouter');
